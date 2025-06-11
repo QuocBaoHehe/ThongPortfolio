c272965806202d557e1d16418eecf5c89abc0140
Quoc Bao <doanquocbao01@gmail.com>
2025-06-12 06:50:36 +0700

    Update content

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -44,3 +44,16 @@ function handleMouseMove(e) {
 
 window.addEventListener("scroll", handleScroll);
 window.addEventListener("mousemove", handleMouseMove);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
+});
